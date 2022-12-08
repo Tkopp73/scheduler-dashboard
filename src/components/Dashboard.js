cd4213b524c55f6611df8894521ca6651cd323af
Tim Kopplin <timkopplin@hotmail.com>
2022-12-08 13:53:30 -0800

    scheduler-dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,13 +1,126 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 import classnames from "classnames";
+import Loading from "./Loading";
+import Panel from "./Panel";
+import {
+	getTotalInterviews,
+	getLeastPopularTimeSlot,
+	getMostPopularDay,
+	getInterviewsPerDay,
+} from "helpers/selectors";
+import { setInterview } from "helpers/reducers";
+
+const data = [
+	{
+		id: 1,
+		label: "Total Interviews",
+		getValue: getTotalInterviews,
+	},
+	{
+		id: 2,
+		label: "Least Popular Time Slot",
+		getValue: getLeastPopularTimeSlot,
+	},
+	{
+		id: 3,
+		label: "Most Popular Day",
+		getValue: getMostPopularDay,
+	},
+	{
+		id: 4,
+		label: "Interviews Per Day",
+		getValue: getInterviewsPerDay,
+	},
+];
 
 class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
+	constructor(props) {
+		super(props);
 
-    return <main className={dashboardClasses} />;
-  }
+		this.state = {
+			loading: true,
+			focused: null,
+			days: [],
+			appointments: {},
+			interviews: {},
+		};
+		this.selectPanel = this.selectPanel.bind(this);
+	}
+
+	componentDidMount() {
+		Promise.all([
+			axios.get("/api/days"),
+			axios.get("/api/appointments"),
+			axios.get("/api/interviewers"),
+		]).then(([days, appointments, interviewers]) => {
+			this.setState({
+				loading: false,
+				days: days.data,
+				appointments: appointments.data,
+				interviewers: interviewers.data,
+			});
+		});
+		const focused = JSON.parse(localStorage.getItem("focused"));
+		if (focused) {
+			this.setState({ focused });
+		}
+		this.socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+		this.socket.onmessage = (event) => {
+			const data = JSON.parse(event.data);
+
+			if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+				this.setState((previousState) =>
+					setInterview(previousState, data.id, data.interview)
+				);
+			}
+		};
+	}
+
+	componentDidUpdate(previousProps, previousState) {
+		if (previousState.focused !== this.state.focused) {
+			localStorage.setItem("focused", JSON.stringify(this.state.focused));
+		}
+	}
+
+	componentWillUnmount() {
+		this.socket.close();
+	}
+
+	selectPanel(id) {
+		this.setState((previousState) => ({
+			focused: previousState.focused !== null ? null : id,
+		}));
+	}
+	render() {
+		console.log("render", this.state);
+		const dashboardClasses = classnames("dashboard", {
+			"dashboard--focused": this.state.focused,
+		});
+
+		const panels = (
+			this.state.focused
+				? data.filter((panel) => this.state.focused === panel.id)
+				: data
+		).map((panel) => {
+			return (
+				<Panel
+					key={panel.id}
+					id={panel.id}
+					label={panel.label}
+					value={panel.getValue(this.state)}
+					onSelect={this.selectPanel}
+				/>
+			);
+		});
+
+		if (this.state.loading) {
+			return <Loading />;
+		}
+
+		return <main className={dashboardClasses}>{panels}</main>;
+	}
 }
 
 export default Dashboard;
